8399ff6d8c51e7a9ee58005f366258c94fb437ac
Ian Clyde E. Tejada <121525736+ClydeTeia@users.noreply.github.com>
2024-11-05 19:23:44 +0800

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,6 @@ const createJestConfig = nextJest({
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
