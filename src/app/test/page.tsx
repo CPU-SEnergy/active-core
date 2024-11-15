5d698e7e79f356bf20da80ee50f6e49a7fe96b95
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-11-15 14:57:36 +0800

    add test route for displaying info

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,27 @@
-"use client"
+"use client";
 
 import { useAuth } from '@/auth/AuthContext';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const { user } = useAuth();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <h1>Welcome to Next.js Firebase Auth Example</h1>
       {user ? (
-        <p>Logged in as: {user.email}</p>
+        <p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+          Logged in as: {user.email}
+        </p>
       ) : (
         <p>You are not logged in.</p>
       )}
