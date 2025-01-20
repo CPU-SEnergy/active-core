79474559dd4e43aded786712d2a5fcf078fa0f0f
Jokkeeerrz <jpp200214@gmail.com>
2025-01-21 01:02:13 +0800

    redo classes page api

--- src/app/api/classes/route.ts
+++ src/app/api/classes/route.ts
@@ -1,29 +1,22 @@
 import { NextResponse } from 'next/server';
 import { getFirebaseAdminApp } from '@/lib/firebaseAdmin';
-import { getFirestore, Query, CollectionReference, DocumentData } from 'firebase-admin/firestore';
+import { getFirestore } from 'firebase-admin/firestore';
+import { ProductType } from '@/lib/types/product';
 
 const db = getFirestore(getFirebaseAdminApp());
 
-export async function GET(req: Request) {
+export async function GET() {
   try {
-    const { searchParams } = new URL(req.url);
-    const coachId = searchParams.get("coach_id")
+    const classesCollection = db.collection(ProductType.CLASSES);
 
-    let query: Query<DocumentData> | CollectionReference<DocumentData> = db.collection('classes');
+    const querySnapshot = await classesCollection.get();
 
-    if (coachId) {
-      query = query.where('coach_id', '==', coachId);
-    }
-
-    const snapshot = await query.get();
-
-    const classes = snapshot.docs.map(doc => ({
+    const classes = querySnapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
-
     console.log(classes)
-    return NextResponse.json(classes);
+    return NextResponse.json(classes)
   } catch (error) {
     console.error("Error fetching classes: ", error);
     return NextResponse.json({ error: "Failed to fetch classes" });
