af95baec98f5e05b426855a9ebe7637e9db1e2c3
Jokkeeerrz <jpp200214@gmail.com>
2025-01-20 20:15:48 +0800

    update coaches page api

--- src/app/api/coaches/route.ts
+++ src/app/api/coaches/route.ts
@@ -1,12 +1,13 @@
 import { app } from '@/lib/firebaseClient';
 import { NextResponse } from 'next/server';
 import { getFirestore, getDocs, collection } from 'firebase/firestore';
+import { ProductType } from '@/lib/types/product';
 
 const db = getFirestore(app);
 
 export async function GET() {
   try {
-    const coachCollection = collection(db, "coach");
+    const coachCollection = collection(db, ProductType.COACHES);
 
 
     const querySnapshot = await getDocs(coachCollection);
