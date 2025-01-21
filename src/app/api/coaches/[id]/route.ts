424aac05734987091d59de13590b63de19b9342a
Jokkeeerrz <jpp200214@gmail.com>
2025-01-21 22:19:26 +0800

    patch dynamic routing for coaches page api

--- src/app/api/coaches/[id]/route.ts
+++ src/app/api/coaches/[id]/route.ts
@@ -13,12 +13,10 @@ export async function GET(req: NextRequest,context: { params: Params }) {
   try {
   const { id } = context.params;
   const numericId = Number(id);
-  console.log(`Fetching coach with ID: ${numericId}`); // Log the ID
   const coachDoc = db.collection(ProductType.COACHES);
   const querySnapshot = await coachDoc.where("id", "==", numericId).get();
 
   if (querySnapshot.empty) {
-    console.log(`Coach with ID ${numericId} not found`); // Log the ID
     return NextResponse.json({ error: "Coach not found" }, { status: 404});
   }
 
@@ -27,7 +25,6 @@ export async function GET(req: NextRequest,context: { params: Params }) {
     ...doc.data(),
   }))[0];
 
-  console.log(coaches);
   return NextResponse.json(coaches);
 
   } catch (error) {
