ea07367783cde349a72d0589ce9770848f15d779
AllanLoydPacete <allanloyd.pacete-20@cpu.edu.ph>
2025-01-24 23:46:27 +0800

    add couches info in classes

---
+++ src/mock_data/coachesMockData.ts
@@ -0,0 +1,15 @@
+const coachesMockData = [
+  {
+    name: "Coach A",
+    bio: "Bio of Coach A",
+    image: "/images/coachA.jpg",
+  },
+  {
+    name: "Coach B",
+    bio: "Bio of Coach B",
+    image: "/images/coachB.jpg",
+  },
+  // Add more coaches as needed
+];
+
+export default coachesMockData;
