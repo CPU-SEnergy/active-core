498391bf3f3d009c074b0d8da24d53f523eedaac
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2025-01-29 00:53:42 +0800

    improve: user dashboard and make fetch user data

--- src/utils/helpers/getUserById.ts
+++ src/utils/helpers/getUserById.ts
@@ -2,7 +2,9 @@ import fetcher from "@/lib/fetcher";
 import useSWR from "swr";
 
 export default function useGetUserById(userId: string) {
-  const { data, error, isLoading } = useSWR(`/api/user/${userId}`, fetcher);
+  const { data, error, isLoading } = useSWR(`/api/user/${userId}`, fetcher, {
+    dedupingInterval: 60000,
+  });
 
   return {
     user: data,
