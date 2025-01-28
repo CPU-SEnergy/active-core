e87c5b3f736ec2e9f97b97328b45dd4335cd9b65
Jokkeeerrz <jpp200214@gmail.com>
2025-01-28 19:41:41 +0800

    update api admin overview kpi route

--- src/app/api/admin/overview/route.ts
+++ src/app/api/admin/overview/route.ts
@@ -1,24 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getFirebaseAdminApp } from "@/lib/firebaseAdmin";
 import { getFirestore } from "firebase-admin/firestore";
-import { KpiMonth } from "@/lib/types/kpi";
 
 const db = getFirestore(getFirebaseAdminApp());
 
 export async function GET(req: NextRequest) {
   try {
-    const kpiMonth = db.collection(KpiMonth.totalRevenue);
+    const monthDocRef = db
+      .collection('kpis')
+      .doc("2025")
+      .collection('months')
+      .doc("01");
 
-    const querySnapshot = await kpiMonth.get();
+    const monthDoc = await monthDocRef.get();
 
-    const kpi = querySnapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-
-    return NextResponse.json(kpi);
+    if (monthDoc.exists) {
+      console.log(`Data for :`, monthDoc.data());
+      return NextResponse.json(monthDoc.data());
+    } else {
+      console.log(`No data found for `);
+      return NextResponse.json({ error: 'No data found' });
+    }
   } catch (error) {
-    console.error("Error fetching kpis: ", error);
-    return NextResponse.json({ error: "Failed to fetch kpis" });
+    console.error('Error fetching data:', error);
+    return NextResponse.error();
   }
 }
