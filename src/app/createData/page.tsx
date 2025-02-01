bd372b19ec272a8ef738357ab089dc2b1fa54a0b
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2025-02-01 20:49:25 +0800

    fix: missing fields in payments doc

--- src/app/createData/page.tsx
+++ src/app/createData/page.tsx
@@ -20,13 +20,14 @@ export default function App() {
   };
 
   console.log(watch("user.name"));
+  console.log(watch("isNewCustomer"), "types", typeof watch("isNewCustomer"));
   console.log(watch("availedPlan.amount"), typeof watch("availedPlan.amount"));
 
   const formatDate = (date: Date) => {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`; // Returns the date in yyyy-mm-dd format
+    return `${year}-${month}-${day}` as unknown as Date; // Returns the date in yyyy-mm-dd format
   };
 
   const [initialized, setInitialized] = useState(false);
@@ -38,9 +39,14 @@ export default function App() {
       const randomDuration =
         durations[Math.floor(Math.random() * durations.length)];
 
-      const startDate = today;
-      const expiryDate = new Date(today);
-      expiryDate.setDate(expiryDate.getDate() + (randomDuration ?? 0));
+      const startDate = today as Date;
+      const expiryDate = new Date(today) as Date;
+      expiryDate.setDate(
+        expiryDate.getDate() + (randomDuration ?? 0)
+      ) as unknown as Date;
+
+      console.log(formatDate(startDate), formatDate(expiryDate), "date");
+      console.log(typeof startDate, typeof expiryDate, "types");
 
       setValue("availedPlan.duration", randomDuration ?? 0);
       setValue("availedPlan.startDate", formatDate(startDate));
@@ -65,7 +71,6 @@ export default function App() {
       {errors.id && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue={"cash"}
         {...register("paymentMethod", { required: true })}
@@ -74,7 +79,6 @@ export default function App() {
       {errors.paymentMethod && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue="M4eOJ2D6nhdJ2z6ZKMsF4HLwQzo1"
         {...register("user.userId", { required: true })}
@@ -83,7 +87,6 @@ export default function App() {
       {errors.user?.userId && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue="Ian Clyde Tejada"
         {...register("user.name", { required: true })}
@@ -92,7 +95,6 @@ export default function App() {
       {errors.user?.name && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue="/pictures/Clyde.png"
         {...register("user.imageUrl", { required: true })}
@@ -101,7 +103,6 @@ export default function App() {
       {errors.user?.imageUrl && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue={crypto.randomUUID()}
         {...register("availedPlan.membershipPlanId", { required: true })}
@@ -110,7 +111,6 @@ export default function App() {
       {errors.availedPlan?.membershipPlanId && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue="testPlanName"
         {...register("availedPlan.name", { required: true })}
@@ -119,7 +119,6 @@ export default function App() {
       {errors.availedPlan?.name && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue={Number(Math.floor(Math.random() * 1000))}
         type="number"
@@ -131,7 +130,6 @@ export default function App() {
       {errors.availedPlan?.amount && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <input
         defaultValue={31}
         type="number"
@@ -141,12 +139,31 @@ export default function App() {
       {errors.availedPlan?.duration && (
         <span className="text-red-500">This field is required</span>
       )}
-
+      <input
+        defaultValue={"pending"}
+        type="text"
+        {...register("status", { required: true })}
+        placeholder="Duration (days)"
+      />
+      {errors.availedPlan?.duration && (
+        <span className="text-red-500">This field is required</span>
+      )}
+      <input
+        type="checkbox"
+        {...register("isNewCustomer", { required: true })}
+        defaultChecked={true}
+        placeholder="Is New Customer"
+      />
+      {errors.availedPlan?.duration && (
+        <span className="text-red-500">This field is required</span>
+      )}
       <label>Start Date</label>
       <input
         type="date"
         {...register("availedPlan.startDate", { required: true })}
-        value={startDateValue ? formatDate(new Date(startDateValue)) : ""}
+        value={
+          startDateValue ? formatDate(new Date(startDateValue)).toString() : ""
+        }
         onChange={(e) =>
           setValue("availedPlan.startDate", new Date(e.target.value))
         }
@@ -154,12 +171,15 @@ export default function App() {
       {errors.availedPlan?.startDate && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <label>Expiry Date</label>
       <input
         type="date"
         {...register("availedPlan.expiryDate", { required: true })}
-        value={expiryDateValue ? formatDate(new Date(expiryDateValue)) : ""}
+        value={
+          expiryDateValue
+            ? formatDate(new Date(expiryDateValue)).toString()
+            : ""
+        }
         onChange={(e) =>
           setValue("availedPlan.expiryDate", new Date(e.target.value))
         }
@@ -167,7 +187,6 @@ export default function App() {
       {errors.availedPlan?.expiryDate && (
         <span className="text-red-500">This field is required</span>
       )}
-
       <button type="submit" className="px-4 py-2 bg-blue-500 text-white">
         Submit
       </button>
