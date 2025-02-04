ab872c5dd943515b7475e49e55b60a6a15914333
Jokkeeerrz <jpp200214@gmail.com>
2025-02-04 10:55:02 +0800

    update recent activity route

--- src/app/api/admin/overview/recent-activity/route.ts
+++ src/app/api/admin/overview/recent-activity/route.ts
@@ -1,25 +1,31 @@
 import { getFirebaseAdminApp } from "@/lib/firebaseAdmin";
 import { db } from "@/lib/schema/firestore";
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
     getFirebaseAdminApp();
 
-    const paymentsCollection = db.payments;
-    const paymentQuery = paymentsCollection.orderBy("createdAt", "desc").limit(5);
-    const paymentDoc = await paymentQuery.get();
+    const paymentsCollection = await db.payments.query(($) =>
+      $.field("createdAt").order("desc")
+    );
 
-    if (!paymentDoc || !paymentDoc.data) {
+    const cleanPayments = paymentsCollection.map((doc) => ({
+      ...doc.data,
+    }));
+
+    if (!cleanPayments) {
       return new Response(JSON.stringify({ error: "Document not found" }), {
         status: 404,
         headers: { "Content-Type": "application/json" },
       });
     }
 
-    return new Response(JSON.stringify(paymentDoc), {
+    console.log(cleanPayments);
+
+    return new Response(JSON.stringify(cleanPayments), {
       status: 200,
       headers: { "Content-Type": "application/json" },
-    })
+    });
   } catch (error) {
     console.error("Error fetching document:", error);
     return new Response(JSON.stringify({ error: "Internal Server Error" }), {
