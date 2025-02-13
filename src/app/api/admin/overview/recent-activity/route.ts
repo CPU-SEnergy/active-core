6be1a7830957a6341322aec595586eaf7360aeec
John Patrick Pineda <143706437+Jokkeeerrz@users.noreply.github.com>
2025-02-13 22:36:05 +0800

    Feature/admin/overview/kpi (#106)
    * add recent activity route
    
    * update recent activity route
    
    * update month comparison for kpi route
    
    * update kpi route to use new schema
    
    * update new schema
    
    * update kpi route and schema
    
    * update kpi route compare functions
    
    * fix kpi route compare functions
    
    * update kpi route
    
    * update kpi route api

---
+++ src/app/api/admin/overview/recent-activity/route.ts
@@ -0,0 +1,36 @@
+import { getFirebaseAdminApp } from "@/lib/firebaseAdmin";
+import { db } from "@/lib/schema/firestore";
+
+export async function GET() {
+  try {
+    getFirebaseAdminApp();
+
+    const paymentsCollection = await db.payments.query(($) =>
+      $.field("createdAt").order("desc")
+    );
+
+    const cleanPayments = paymentsCollection.map((doc) => ({
+      ...doc.data,
+    }));
+
+    if (!cleanPayments) {
+      return new Response(JSON.stringify({ error: "Document not found" }), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    console.log(cleanPayments);
+
+    return new Response(JSON.stringify(cleanPayments), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching document:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
