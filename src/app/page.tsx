f9d99086f76add1239575de1efbd1ad68b4a1095
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2025-02-15 22:07:14 +0800

    add: chat system for customers to s&f admins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,4 +4,4 @@ export default async function Page() {
   return (
     <Home />
   )
-} 
+}
--- src/lib/firebaseClient.ts
+++ src/lib/firebaseClient.ts
@@ -1,5 +1,8 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { clientConfig } from "@/lib/config";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 export const app = (() => {
   if (!clientConfig || Object.keys(clientConfig).length === 0) {
@@ -20,3 +23,7 @@ export const app = (() => {
 
   return getApp();
 })();
+
+export const database = getDatabase(app);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
