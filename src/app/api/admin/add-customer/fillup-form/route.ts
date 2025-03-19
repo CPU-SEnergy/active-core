4f07ab169b5357cc7e7eb28e0fe77aee5a4cdc63
Jokkeeerrz <143706437+Jokkeeerrz@users.noreply.github.com>
2025-03-19 12:51:24 +0800

    update fillup form route

--- src/app/api/admin/add-customer/fillup-form/route.ts
+++ src/app/api/admin/add-customer/fillup-form/route.ts
@@ -1,17 +1,38 @@
 import { getFirebaseAdminApp } from "@/lib/firebaseAdmin";
-import { FieldValue } from "firebase-admin/firestore"
+import { db } from "@/lib/schema/firestore";
+
+interface Customer {
+  uid: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  sex: string;
+  dob: Date;
+  membershipPlan: string;
+}
 
 export async function POST(req: Request) {
   try {
-    const data = await req.json();
-    const {
-      email,
-      uid,
-      role = "customer",
-      firstName,
-      lastName,
-      dob,
-    } = data;
+    getFirebaseAdminApp();
+
+    const data: Customer = await req.json();
+
+    if (!data.uid || !data.firstName || !data.lastName || !data.email || !data.sex || !data.dob || !data.membershipPlan) {
+      return new Response(JSON.stringify({ error: "Invalid data" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const newCustomer = {
+      ...data,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      isCustomer: true,
+      
+    }
+
+    await db.users.add(newCustomer);
   } catch (error) {
     console.error("Error fetching document: ", error);
     return new Response(JSON.stringify({ error: "Internal Server Error" }), {
