06fb03d6735a9d8ea1951d1f1777ad42839d753c
Jokkeeerrz <143706437+Jokkeeerrz@users.noreply.github.com>
2025-03-24 14:09:46 +0800

    update cashier tagging

--- src/app/api/admin/add-cashier/cashier-tagging/route.ts
+++ src/app/api/admin/add-cashier/cashier-tagging/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { refreshNextResponseCookies } from 'next-firebase-auth-edge/lib/next/cookies';
 import { getFirebaseAuth, getTokens } from 'next-firebase-auth-edge';
 import { serverConfig } from '@/lib/config';
+import { getFirebaseAdminApp } from '@/lib/firebaseAdmin';
+import { FieldValue } from 'firebase-admin/firestore';
 
 const { setCustomUserClaims, getUser } = getFirebaseAuth({
   serviceAccount: serverConfig.serviceAccount,
@@ -9,20 +11,23 @@ const { setCustomUserClaims, getUser } = getFirebaseAuth({
 });
 
 export async function POST(request: NextRequest) {
+  try {  
+  getFirebaseAdminApp();
+
+  const db = getFirebaseAdminApp().firestore();
+
   const tokens = await getTokens(request.cookies, serverConfig);
 
   if (!tokens) {
     throw new Error('Cannot update custom claims of unauthenticated user');
   }
 
-  const { targetUid } = await request.json()
+  const { targetUid } = await request.json();
 
   if (!targetUid) {
     return new NextResponse(
-      JSON.stringify({ error: 'Target UID is required' }), {
-        status: 400,
-        headers: { 'content-type': 'application/json' }
-      } 
+      JSON.stringify({ error: 'Target UID is required' }),
+      { status: 400, headers: { 'content-type': 'application/json' } }
     );
   }
 
@@ -32,16 +37,29 @@ export async function POST(request: NextRequest) {
 
   const user = await getUser(targetUid);
   console.log('User custom claims updated', user!.customClaims);
+
+  await db.collection('cashier').doc(targetUid).set({
+    uid: targetUid,
+    createdAt: FieldValue.serverTimestamp(),
+  })
+ 
   const response = new NextResponse(
     JSON.stringify({
       customClaims: user!.customClaims,
     }), {
       status: 200,
-      headers: { 'content-type': 'application/json'}
+      headers: { 'content-type': 'application/json' }
     }
   );
 
   console.log(await getUser(targetUid));
 
   return refreshNextResponseCookies(request, response, serverConfig);
+  } catch (error) {
+    console.error('Error adding cashier role: ', error);
+    return new NextResponse(
+      JSON.stringify({ error: 'Internal Server Error' }),
+      { status: 500, headers: { 'content-type': 'application/json' } }
+    );
+  }
 }
