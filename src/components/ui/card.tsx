7ab876c1c8c36c194efc65ac815489420e902c4a
Chito Miguel <chitomiguel.poral-21@cpu.edu.ph>
2025-05-15 18:29:03 +0800

    changes in the UI of user profile

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+function cn(...classes: (string | undefined)[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export const Card: React.FC<{
   className?: string;
@@ -31,3 +34,27 @@ export const CardContent: React.FC<{
 }> = ({ className, children }) => {
   return <div className={`p-4 ${className}`}>{children}</div>;
 };
+
+export const CardDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("text-sm text-muted-foreground", className)}
+    {...props}
+  />
+))
+CardDescription.displayName = "CardDescription"
+ 
+export const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items-center p-6 pt-0", className)}
+    {...props}
+  />
+))
+CardFooter.displayName = "CardFooter"
