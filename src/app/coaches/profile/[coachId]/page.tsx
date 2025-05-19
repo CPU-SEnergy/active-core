484698ed420fb20e6743890f351af560b511e8e1
Rhea Rizz Perocho <rizzperocho@gmail.com>
2025-05-19 17:58:57 +0800

    get rid of the footer import in coaches profile

--- src/app/coaches/profile/[coachId]/page.tsx
+++ src/app/coaches/profile/[coachId]/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image"; 
 import { coaches } from "@/lib/mock_data/coachesMockData";
-import Footer from "@/components/Footer";
 
 export default function CoachTest({ params }: { params: { coachId: number } }) {
   const coach = coaches[params.coachId];
