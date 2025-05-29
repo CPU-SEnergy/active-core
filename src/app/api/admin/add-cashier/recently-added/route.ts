b56195f21b7e43c343fd58ab653f3f671fe2ac64
Jokkeeerrz <143706437+Jokkeeerrz@users.noreply.github.com>
2025-05-30 01:45:12 +0800

    refactor recently-added cashier route

--- src/app/api/admin/add-cashier/recently-added/route.ts
+++ src/app/api/admin/add-cashier/recently-added/route.ts
@@ -1,24 +1,25 @@
 import { getFirebaseAdminApp } from "@/lib/firebaseAdmin";
+import { db } from "@/lib/schema/firestore";
 
 export async function GET() {
   try {
-    const adminApp = getFirebaseAdminApp();
-    const db = adminApp.firestore();
+    getFirebaseAdminApp();
+    const cashiersCollection = await db.cashier.query(($) => 
+      $.field("createdAt").order("desc")
+    )
 
-    const cashiersSnapshot = await db.collection('cashier').get();
-    const cashiers = cashiersSnapshot.docs.map(doc => doc.data());
-    
-
-    return new Response(JSON.stringify(cashiers), {
+    return new Response(JSON.stringify(cashiersCollection), {
       status: 200,
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" }
     });
 
   } catch (error) {
-    console.error("Error fetching document: ", error);
-    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+    console.error("Error fetching cashiers:", error);
+    return new Response(JSON.stringify({ 
+      error: error instanceof Error ? error.message : "Internal Server Error" 
+    }), {
       status: 500,
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "application/json" }
     });
   }
 }
